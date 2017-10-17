4ea23d1aae00445c3ea643ef72ca284b9728fbe6
Christian Dohnert <donut87@gmail.com>
2017-10-17 20:52:28 +0200

    implementing stage 2

--- src/actor/SuperMario.java
+++ src/actor/SuperMario.java
@@ -3,14 +3,26 @@ package actor;
 public class SuperMario {
 	
 	private boolean isDead;
+	private boolean isBig;
 
 	public void isHitByEnemy() {
-		// TODO Auto-generated method stub
+		if(isBig) {
+			isBig = false;
+			return;
+		}
 		isDead = true;
 	}
 
 	public boolean isDead() {
 		return isDead;
+	}
+
+	public void findsMushroom() {
+		isBig = true;
+	}
+
+	public boolean isBig() {
+		return isBig;
 	}
 
 }
--- test/actor/SuperMarioTest.java
+++ test/actor/SuperMarioTest.java
@@ -7,18 +7,35 @@ import org.junit.Test;
 import actor.SuperMario;
 
 public class SuperMarioTest {
-	
+
 	@Test
 	public void WhenSmallMarioIsHitByEnemy_ThenHeDies() throws Exception {
 		SuperMario mario = new SuperMario();
 		mario.isHitByEnemy();
 		assertTrue(mario.isDead());
 	}
-	
+
 	@Test
 	public void WhenMarioIsInitialized_ThenHeIsAlive() throws Exception {
 		SuperMario mario = new SuperMario();
 		assertFalse(mario.isDead());
+	}
+
+	@Test
+	public void WhenMarioFindsMushroom_ThenHeIsBig() throws Exception {
+		SuperMario mario = new SuperMario();
+		mario.findsMushroom();
+		assertTrue(mario.isBig());
+	}
+
+	@Test
+	public void WhenBigMarioIsHitByEnemy_ThenHeBecomesSmallMario() throws Exception {
+		SuperMario mario = new SuperMario();
+		mario.findsMushroom();
+
+		mario.isHitByEnemy();
+
+		assertFalse(mario.isBig());
 	}
 
 }
