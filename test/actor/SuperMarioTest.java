e69f47dbf686655bf8be4d4939b1d11f61d0ce99
Christian Dohnert <donut87@gmail.com>
2017-10-17 21:04:36 +0200

    remove obsolete test

--- test/actor/SuperMarioTest.java
+++ test/actor/SuperMarioTest.java
@@ -9,13 +9,6 @@ import actor.SuperMario;
 public class SuperMarioTest {
 
 	@Test
-	public void WhenSmallMarioIsHitByEnemy_ThenHeDies() throws Exception {
-		SuperMario mario = new SuperMario();
-		mario.isHitByEnemy();
-		assertTrue(mario.isDead());
-	}
-
-	@Test
 	public void WhenMarioIsInitialized_ThenHeIsAlive() throws Exception {
 		SuperMario mario = new SuperMario();
 		assertFalse(mario.isDead());
