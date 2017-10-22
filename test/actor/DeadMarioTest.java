f29e5eaa108992dd91c1c313af21ac501fb370b7
Christian Dohnert <donut87@gmail.com>
2017-10-22 10:28:21 +0200

    Pumping up the coverage to 100%

--- test/actor/DeadMarioTest.java
+++ test/actor/DeadMarioTest.java
@@ -1,24 +1,58 @@
 package actor;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class DeadMarioTest {
-
     
+    IamSuperMario mario;
+
+    @Before
+    public void setup() {
+        mario = new DeadMario();
+    }
 
     @Test
     public void WhenDeadSuperMarioFindsLive_HeBecomesSmallMario() throws Exception {
-        IamSuperMario mario = new DeadMario();
-
         mario = mario.findsLive();
 
         assertTrue(SmallMario.class.isAssignableFrom(mario.getClass()));
     }
 
+    @Test
+    public void DeadMarioHasNoLives() throws Exception {
+        assertEquals(0, mario.getLives());
+    }
 
+    @Test
+    public void WhenDeadMarioIsHitByEnemy_ThenHeStaysDead() throws Exception {
+        mario = mario.isHitByEnemy();
 
+        assertTrue(DeadMario.class.isAssignableFrom(mario.getClass()));
+    }
 
+    @Test
+    public void WhenDeadMarioFindsMushroom_ThenHeStaysDead() throws Exception {
+        mario = mario.findsMushroom();
+
+        assertTrue(DeadMario.class.isAssignableFrom(mario.getClass()));
+    }
+
+    @Test
+    public void WhenDeadMarioFindsFireFlower_ThenHeStaysDead() throws Exception {
+        mario = mario.findsFireFlower();
+
+        assertTrue(DeadMario.class.isAssignableFrom(mario.getClass()));
+    }
+
+    @Test
+    public void WhenDeadMarioFindsIceFlower_ThenHeStaysDead() throws Exception {
+        mario = mario.findsIceFlower();
+
+        assertTrue(DeadMario.class.isAssignableFrom(mario.getClass()));
+    }
 
 }
